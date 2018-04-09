3ecbcc3cc51804e5f5f7d1d1be4f9f2fb322582c
eoinm <eoinm@al.com.au>
2018-04-09 13:26:43 +1000

    "Documentation for AL_USDMaya-0.27.5."

---
+++ group__usdmaya.js
@@ -0,0 +1,23 @@
+var group__usdmaya =
+[
+    [ "Nodes", "group__nodes.html", "group__nodes" ],
+    [ "Commands", "group__commands.html", "group__commands" ],
+    [ "File IO", "group__fileio.html", "group__fileio" ],
+    [ "Global", "classAL_1_1usdmaya_1_1Global.html", null ],
+    [ "StageCache", "classAL_1_1usdmaya_1_1StageCache.html", null ],
+    [ "StageData", "classAL_1_1usdmaya_1_1StageData.html", [
+      [ "StageData", "classAL_1_1usdmaya_1_1StageData.html#a04130488583cba5b5c42b24a27412d4c", null ],
+      [ "~StageData", "classAL_1_1usdmaya_1_1StageData.html#a8ef5febd761b20d625d9cb5ef0f66370", null ],
+      [ "copy", "classAL_1_1usdmaya_1_1StageData.html#a0050dcdeb8163ce90215f25acfe6c869", null ],
+      [ "primPath", "classAL_1_1usdmaya_1_1StageData.html#af8215cdbf77c70329982f13421d9479e", null ],
+      [ "stage", "classAL_1_1usdmaya_1_1StageData.html#aa736bf6ddb75fb77906a17c29ff5f179", null ]
+    ] ],
+    [ "TransformOperation", "group__usdmaya.html#ga2c3a800f8931e38ab4e10b0cf2ec6980", null ],
+    [ "UsdDataType", "group__usdmaya.html#gabe4c70835b957dc08bf40e0c229cd236", null ],
+    [ "getAttributeType", "group__usdmaya.html#gafaaa852cec17128e2fb5f352e114d104", null ],
+    [ "getAttributeType", "group__usdmaya.html#ga77c4e54d7ba9cf97e1f4d33a0b056c66", null ],
+    [ "matchesMayaProfile", "group__usdmaya.html#ga47c8b3ff22cb0d44be0e57dd95fb202f", null ],
+    [ "registerPlugin", "group__usdmaya.html#gaa4edec5f87980cd4a7a84c60b4c832f1", null ],
+    [ "unregisterPlugin", "group__usdmaya.html#ga03d499eef4ae1302dde337fc499f915a", null ],
+    [ "xformOpToEnum", "group__usdmaya.html#gaa18901b4e898f79b7e0e0dfcd6e4b9ed", null ]
+];
